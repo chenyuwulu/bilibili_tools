7a843999d56392369fa2415149a1b8be9637ec35
尘雨 <675687771@qq.com>
2021-07-02 19:41:20 +0800

    尝试调整开头注入

--- js/content-start.js
+++ js/content-start.js
@@ -1,4 +1,4 @@
-﻿// (function() {
+// (function() {
 // 	console.log('这是 simple-chrome-plugin-demo 的content-script！');
 // 	// console.log('这是jquery',$("#app").hide())
 // 	$("#app").children(".bili-wrapper").children("#chief_recommend").hide();
@@ -10,4 +10,22 @@
 // function xxx(e){
 //   callback(2)
 // }
-// console.log(xxx(4))
+console.log("start js位置")
+$.post("https://manga.bilibili.com/twirp/user.v1.User/GetAutoBuyComics?device=pc&platform=web",{
+	page_num: 2,
+	page_size: 100
+},
+(data)=>{
+	console.log(data)
+});
+$.post("https://apidoc.chenyuwulu.top/mock/86/uniapp/app/login",{
+	username: 2,
+	password: 100
+},
+(data)=>{
+	console.log(data)
+});
+// setTimeout(()=>{
+// 	let box= document.getElementsByClassName("purchased-comic-item");
+// 	console.log(box[0].innerHTML)
+// },2000)
